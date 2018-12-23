9fbdc6be21fcb5ded2c626875d66f40b61c68cf5
makozi <marizumakozi97@gmail.com>
2018-12-23 23:21:09 +0100

    update

--- js/popup-onload.js
+++ js/popup-onload.js
@@ -11,14 +11,20 @@
 //      });
 
 
-$(function () {
-    var overlay = $('<div id="overlay"></div>');
-    overlay.show();
-    overlay.appendTo(document.body);
-    $('.popup').show();
-    $('.x').click(function () {
-        $('.popup').hide();
-        overlay.appendTo(document.body).remove();
-        return false;
-    });
+// $(function () {
+//     var overlay = $('<div id="overlay"></div>');
+//     overlay.show();
+//     overlay.appendTo(document.body);
+//     $('.popup').show();
+//     $('.x').click(function () {
+//         $('.popup').hide();
+//         overlay.appendTo(document.body).remove();
+//         return false;
+//     });
+// });
+
+
+$(document).ready(function() {
+    $('#myModal').modal('show');
+ 
 });
